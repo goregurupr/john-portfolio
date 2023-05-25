db49b068172a3c0e48c2722390fee8963b6e897b
prasad <prasad@gamil.com>
2023-05-25 12:33:39 +0530

    error boundary add

---
+++ app/contact/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Spinner from '../components/Spinner'
+
+const loading = () => {
+  return <>
+      <Spinner/>
+  </>
+
+}
+
+export default loading
---
+++ app/projects/error.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const error = () => {
+  return <>
+    <h1>Something Went wrong</h1>
+  </>
+}
+
+export default error
